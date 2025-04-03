f2be2a83b3537fd2a045b6ddc9b61668881139c3
aryankanani28 <arkanani28@gmail.com>
2025-04-03 10:23:14 +0530

    Update longest-consequtive-sequence.cpp

--- longest-consequtive-sequence.cpp
+++ longest-consequtive-sequence.cpp
@@ -4,17 +4,31 @@ public:
         unordered_map<int, int> map;
         int longestStreak = 0;
 
-        for (int num : nums) {
-            if (map.find(num) == map.end()) {
-                int left = (map.find(num -1) != map.end()) ? map[num- 1] : 0;
-                int right = (map.find(num+1) != map.end()) ? map[num + 1] : 0;
-                int sequenceLength = left+1 + right;
+        for (int i = 0; i < nums.size(); i++) {
+            if (map.find(nums[i]) == map.end()) {
+                int left, right;
 
-                map[num] = sequenceLength;
-                map[num-left] =sequenceLength;
-                map[num+right]=sequenceLength;
+                if (map.find(nums[i] - 1) != map.end()) {
+                    left = map[nums[i] - 1];
+                } else {
+                    left = 0;
+                }
 
-                longestStreak =max(longestStreak, sequenceLength);
+                if (map.find(nums[i] + 1) != map.end()) {
+                    right = map[nums[i] + 1];
+                } else {
+                    right = 0;
+                }
+
+                int sequenceLength = left + 1 + right;
+
+                map[nums[i]] = sequenceLength;
+                map[nums[i] - left] = sequenceLength;
+                map[nums[i] + right] = sequenceLength;
+
+                if (sequenceLength > longestStreak) {
+                    longestStreak = sequenceLength;
+                }
             }
         }
 
